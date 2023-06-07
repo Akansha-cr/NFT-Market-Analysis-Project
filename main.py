c2a3565ce31368e00c53769e437e55d409775405
Akansha <av0753484@gmail.com>
2023-06-07 13:18:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -50,3 +50,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+    #background image
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('Untitled design.png')
